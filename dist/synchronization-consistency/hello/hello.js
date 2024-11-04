836b2aed7e3a8cfd5f5f59d84080606f4d1196f2
Janish Suneja <js2629@njit.edu>
2024-11-04 05:32:28 -0500

    Clean branch to push to AlaraBread dev

--- dist/synchronization-consistency/hello/hello.js
+++
@@ -1,8 +0,0 @@
-import { helloworld } from "../../generated/hello.js";
-export const hello = (request, respond) => {
-    const message = `hello, ${request.request.name}!!!`;
-    console.log(message);
-    respond(null, new helloworld.HelloReply({
-        message,
-    }));
-};
